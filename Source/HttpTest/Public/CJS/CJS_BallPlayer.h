72ac313ac17c3c9bcc4688881fec059fc91eef19
Cvely <84121069+c-vely@users.noreply.github.com>
2024-10-21 22:46:51 +0900

    <Feat> BallPlayer 추가
    - wxad, space 기능 완료
    - 부딪혔을 때, 파티클 + 소리 완료
    - 하트 던지기 작업 중

---
+++ Source/HttpTest/Public/CJS/CJS_BallPlayer.h
@@ -0,0 +1,71 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+#include "CJS_BallPlayer.generated.h"
+
+UCLASS()
+class HTTPTEST_API ACJS_BallPlayer : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ACJS_BallPlayer();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// 카메라
+	UPROPERTY(EditDefaultsOnly)
+	class USpringArmComponent* SpringArmComp;
+	UPROPERTY(EditDefaultsOnly)
+	class UCameraComponent* CameraComp;
+	FVector Direction; 
+
+	// 인풋
+	UPROPERTY(EditDefaultsOnly)
+	class UInputMappingContext* IMC_Player;
+	UPROPERTY(EditDefaultsOnly)
+	class UInputAction* IA_Move;
+	UPROPERTY(EditDefaultsOnly)
+	class UInputAction* IA_Look;
+	UPROPERTY(EditDefaultsOnly)
+	class UInputAction* IA_Jump;
+	UPROPERTY(EditDefaultsOnly)
+	class UInputAction* IA_Throw;
+	void OnMyActionMove(const FInputActionValue& Value);
+	void OnMyActionLook(const FInputActionValue& Value);
+	void OnMyActionJump(const FInputActionValue& Value);
+	void OnMyActionThrow(const FInputActionValue& Value);
+
+
+	// 부딪혔을 때 ==============================================================================================
+	UPROPERTY(EditDefaultsOnly, Category = "FX")
+	class UParticleSystem* HitVFXFactory;
+	UPROPERTY(EditDefaultsOnly, Category = "FX")
+	class UParticleSystem* SelfHitVFXFactory;
+	UPROPERTY(EditDefaultsOnly, Category = "FX")
+	class USoundBase* HitSFX;
+	void NotifyHit(UPrimitiveComponent* MyComp, AActor* Other, UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit);
+	void TriggerSelfHitEffects(FVector HitLocation);
+
+
+	// 하트 던질 때 ============================================================================================
+	/*UPROPERTY(EditDefaultsOnly, Category = "Heart")
+	TSubclassOf<APalSphereActor> HeartFactory;*/
+
+
+};
